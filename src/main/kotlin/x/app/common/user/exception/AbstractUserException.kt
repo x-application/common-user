b8400096568434c252d16dd4aea93b57cb121356
Jiahua.Zhang <i@iamee.me>
2019-04-27 23:35:02 +0800

    add AbstractUserException

---
+++ src/main/kotlin/x/app/common/user/exception/AbstractUserException.kt
@@ -0,0 +1,24 @@
+package x.app.common.user.exception
+
+import x.app.common.AbstractException
+import x.app.common.user.UserIdentifier
+
+/**
+ *   @Project: common-user
+ *   @Package: x.app.common.user.exception
+ *   @Author:  Iamee
+ *   @Date:    2019-04-27 23:32
+ */
+abstract class AbstractUserException : AbstractException, UserIdentifier {
+
+    final override var userId: String = ""
+
+    constructor()
+
+    constructor(userId: String) : super(userId)
+
+    constructor(userId: String, message: String) : super(message) {
+        this.userId = userId
+    }
+
+}
